851e0198af61181a11947d27ef63d94eb713bc8c
Takahito Iwase <hito.0525@gmail.com>
2016-09-18 21:24:13 +0900

    indexアクション及びpicturesのマイグレーションまで

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+
+resources :pictures
+
+#get 'pictures' => 'pictures#index'
+
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
+
 end
