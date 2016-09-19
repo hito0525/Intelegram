4eb47ac6a5d5e7c0d553bd2cdee1e42dd8d58054
Takahito Iwase <hito.0525@gmail.com>
2016-09-19 19:34:42 +0900

    indexアクションで保存した、値を表示させるところまで

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,12 +1,26 @@
 class PicturesController < ApplicationController
   def index
+    @pictures = Picture.all
   end
 
   def new
+
     @picture = Picture.new
   end
 
   def create
+    Picture.create(pictures_params)
+    #@picture = Picture.new(pictures_params)
+    redirect_to pictures_path
+
+  end
+
+  def destroy
+  end
+
+private
+  def pictures_params
+    params.require(:picture).permit(:title, :content)
   end
 
 end
