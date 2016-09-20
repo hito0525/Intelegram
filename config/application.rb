8ccfd77f891639f4396d359538957076bc9f7e8a
Takahito Iwase <hito.0525@gmail.com>
2016-09-20 21:10:10 +0900

    エラーの元になっているエラー出力タグを表示させないようにした

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,8 @@ module Intelegram
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.active_record.raise_in_transactional_callbacks = true
+
+    config.action_view.field_error_proc = proc { |html_tag, instance| html_tag }
   end
 end
