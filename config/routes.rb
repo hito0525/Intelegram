498a390da6b832fa1b859a5d24e3aabc234f728b
Takahito Iwase <hito.0525@gmail.com>
2016-09-23 10:19:03 +0900

    confirm routing定義

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
 
-resources :pictures, only: [:index, :show, :new, :create, :edit, :update, :destroy]
+resources :pictures, only: [:index, :show, :new, :create, :edit, :update, :destroy] do
+  collection do
+      post :confirm
+    end
+  end
+
 
 #get 'pictures' => 'pictures#index'
 
